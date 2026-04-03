6e608fa3f96a7c3602a51f171528864860fdd2bc
Yash-gitthub <yashbodake8@gmail.com>
2026-04-04 00:51:51 +0530

    Engine - Added check logic for wall collusion

--- roguelike-vscode/src/game/engine.ts
+++ roguelike-vscode/src/game/engine.ts
@@ -43,19 +43,19 @@ start(): void {
       return;
     }
     if ((data == KEY.right || data == KEY.d) && this.state.playerX < this.state.map.width - 1 && 
-    this.state.map.getTile(this.state.playerX + 1, this.state.playerY) === '.'){
+    this.state.map.getTile(this.state.playerX + 1, this.state.playerY).glyph === '.'){
       this.state.playerX += 1;
     }
     if ((data == KEY.left || data == KEY.a) && this.state.playerX > 0 &&
-    this.state.map.getTile(this.state.playerX - 1, this.state.playerY) === '.'){
+    this.state.map.getTile(this.state.playerX - 1, this.state.playerY).glyph === '.'){
       this.state.playerX -= 1;
     }
     if ((data == KEY.up || data == KEY.w) && this.state.playerY > 0 &&
-    this.state.map.getTile(this.state.playerX, this.state.playerY - 1) === '.'){
+    this.state.map.getTile(this.state.playerX, this.state.playerY - 1).glyph === '.'){
       this.state.playerY -= 1;
     }
     if ((data == KEY.down || data == KEY.s) && this.state.playerY < this.state.map.height - 1 &&
-    this.state.map.getTile(this.state.playerX, this.state.playerY + 1) === '.'){
+    this.state.map.getTile(this.state.playerX, this.state.playerY + 1).glyph === '.'){
       this.state.playerY += 1;
     }
     this.render()
