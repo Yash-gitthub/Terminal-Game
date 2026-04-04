0562e14c10b695f6c8e8592a1436cf7ac450b5d3
Yash-gitthub <yashbodake8@gmail.com>
2026-04-04 20:34:49 +0530

    function for distance between two points

---
+++ roguelike-vscode/src/world/fov.ts
@@ -0,0 +1,17 @@
+import { GameState } from "../game/state";
+import { GameMap } from "./map";
+
+export function computeFOV(state: GameState, radius: number): void {
+    const 
+    for (let y = 0; y < state.map.height; y+){
+        for (let x = 0; x < state.map.width; x++){
+            if (){
+                
+            }
+        }
+    }
+}
+
+function distance(x1:number, y1: number, x2: number, y2:number){
+    return ((x2 - x1)**2) + ((y2 - y1)**2);
+}
