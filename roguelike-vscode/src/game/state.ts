b6718b39268454f7fea7dbbb59e34d6f7e2975c5
Yash-gitthub <yashbodake8@gmail.com>
2026-04-05 01:15:28 +0530

    Connecting monster to state

--- roguelike-vscode/src/game/state.ts
+++ roguelike-vscode/src/game/state.ts
@@ -1,10 +1,13 @@
 import { GameMap } from '../world/map';
 import { generateMap } from '../world/mapgen';
+import { Monster } from '../entities/monster';
 
 export class GameState {
   map: GameMap;
   playerX: number;
   playerY: number;
+  playerHp: number;
+  monsters: Monster[];
 
   constructor() {
     this.map = new GameMap(40, 20);
@@ -13,5 +16,11 @@ export class GameState {
     const start = rooms[0].centre();
     this.playerX = start.x;
     this.playerY = start.y;
+    this.playerHp = 30;
+
+    this.monsters = rooms.slice(1).map(room => {
+      const c = room.centre();
+      return new Monster(c.x, c.y, 'g');
+    });
   }
 }
